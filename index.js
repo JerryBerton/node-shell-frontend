60e4b75bc22bc1073db35cb0b816674c5b1c78cf
王敬博 <470665207@qq.com>
2017-06-19 20:58:25 +0800

    更新脚本的钩子

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler');
-var handler = createHandler({ path: '/', secret: 'jerryberton' }) // single handler
+'use staric';
+const  http           = require('http')
+const  createHandler  = require('github-webhook-handler');
+const  handler      = createHandler({ path: '/', secret: 'jerryberton' })
 
 function execFunc(content) {
-  var exec = require('child_process').exec
-  exec(content, function(error, stdout, stderr) {
+  const exec = require('child_process').exec;
+  exec(content, (error, stdout, stderr) => {
     if (error) {
       console.error('exec error:' + error)
-      return
+      return;
     }
     console.log('stdout:' + stdout)
     console.log('stderr:' + stderr)
-  })
+  });
 }
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
+http.createServer((req, res) => {
+  handler(req, res, (err) => {
+    res.statusCode = 404;
+    res.end('no such location');
   })
-}).listen(7777, function() {
+}).listen(7777, () => {
   console.log('webhook is listen ...');
 })
 
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
+handler.on('error',  (err) => {
+  console.error('Error:', err.message);
 })
 
-handler.on('push', function (event) {
+handler.on('push', (event) => {
   console.log(
     'Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref
-  )
+     event.payload.repository.name,
+     event.payload.ref,
+     event.payload
+  );
   //execFunc('sh ./deploy.sh')
 })
