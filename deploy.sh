528926c8b8fd71f40ea3f1c732aacbf51884f9bf
JerryBertonう <jerryberton.wang@gmail.com>
2017-07-04 17:44:34 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-WEB_PATH='/root/tools/'$1
-WEB_USER='root'
-WEB_USERGROUP='root'
-
 echo "Start deployment"
 cd ~
 echo "Enter the root directory"
-cd jerryberton
+cd project/dh-component
 echo "Enter the project directory"
 git reset --hard origin/master
 git clean -f
